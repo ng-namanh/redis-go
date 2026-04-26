bc18dff51ab3b4c4adb14df19181751a398be7ba
ng-namanh <namanh200313@gmail.com>
2026-04-26 15:19:40 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -20,7 +20,10 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
+
+	conn.Write([]byte("+PONG\r\n"))
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
